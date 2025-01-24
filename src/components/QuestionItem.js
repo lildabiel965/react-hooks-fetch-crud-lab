eeb6d1d9b0dfa3e3e2177816fe306b0f08254ee0
Daniel <daniel.mutembei@student.moringaschool.com>
2025-01-24 10:49:44 +0300

    Done

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,13 +1,36 @@
 import React from "react";
+import { fetch } from "whatwg-fetch";
 
 function QuestionItem({ question }) {
   const { id, prompt, answers, correctIndex } = question;
 
+  const handleUpdate = (index) => {
+    
+    fetch(`http://localhost:4000/questions/${id}`,{
+      method:"PATCH",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({...question, correctIndex: Number(index)})
+    }).then((res) => res.json()).then((data) => console.log(data))
+  }
+
   const options = answers.map((answer, index) => (
-    <option key={index} value={index}>
+    <option key={index} value={index} onChange={() => handleUpdate(index)}>
       {answer}
     </option>
   ));
+
+  const handleDelete = () => {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    }).then((res) => res.json()).then((data) => console.log(data))
+  }
+
+
 
   return (
     <li>
@@ -15,9 +38,9 @@ function QuestionItem({ question }) {
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex} onChange={(e) => handleUpdate(e.target.value)}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={() => handleDelete()}>Delete Question</button>
     </li>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, {useEffect,useState} from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:4000/questions', {
+      headers:{
+        'Content-Type': 'application/json'
+      }
+    }).then((res) => res.json()).then((data) => setQuestions(data))
+  }, [])
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{/* display QuestionItem components here after fetching */}
+        {questions.map((question) => <QuestionItem question={question}/>)}
+      </ul>
     </section>
   );
 }
